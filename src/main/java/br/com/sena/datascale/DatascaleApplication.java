5dbbd44c252c3559fd9daac9c235560105f103f7
henrique.sena <carloshenrique3250@gmail.com>
2026-09-16 21:28:48 -0300

    feat: iniciando projeto

---
+++ src/main/java/br/com/sena/datascale/DatascaleApplication.java
@@ -0,0 +1,13 @@
+package br.com.sena.datascale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatascaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatascaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sena/datascale/DatascaleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sena.datascale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatascaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
